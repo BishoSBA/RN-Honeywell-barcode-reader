e9abbad9552f3e6cfcca70ce46fe1a5b22788002
Beshir Saeed <Beshir-saeed@live.com>
2025-06-17 10:03:45 +0300

    Codgen Bug Fix (#3)

--- react-native.config.js
+++ react-native.config.js
@@ -5,10 +5,6 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        sourceDir: '../android',
-        packageImportPath:
-          'import com.honeywell.HoneywellBarcodeReaderPackage;',
-        libraryName: 'react_codegen_RNHoneywellSpec',
         cmakeListsPath: 'generated/jni/CMakeLists.txt',
       },
     },
